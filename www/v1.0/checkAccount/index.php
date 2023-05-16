48085efa8ff87a892ec339bf2e5ce7b13296ef6f
kmg <flyminggo@naver.com>
2023-05-16 11:59:10 +0900

    update bank

--- www/v1.0/checkAccount/index.php
+++ www/v1.0/checkAccount/index.php
@@ -1,58 +1,39 @@
 <?php
 include dirname(__file__) . "/../../lib/TradeApi.php";
-/*$tradeapi->checkLogin();
-$userno = $tradeapi->get_login_userno();
 
-$tradeapi->checkLogin();
-$userno = $tradeapi->get_login_userno();
-$tradeapi->write_log("REQUEST: " . json_encode($_REQUEST));
-*/
-
+// 로그인 세션 확인.
 $tradeapi->checkLogin();
 $userno = $tradeapi->get_login_userno();
 
 $_REQUEST['userno'] = $userno;
 
-$res = "";
-
-// 접속할 IP와 포트를 지정합니다.
 $ip = '61.109.249.165';
 $port = 30576;
-
-// TCP/IP 소켓을 엽니다.
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-
-// 소켓이 열리지 않으면 오류를 출력합니다.
-if ($socket === false) {
-    //$res =  "socket_create() failed: " . socket_strerror(socket_last_error()) . "\n";
-    $this->error('055', __('Failed to connect to coin server. details: ').socket_strerror(socket_last_error());
-}
-
-// 서버에 연결합니다.
-$result = socket_connect($socket, $ip, $port);
-
-// 연결이 실패하면 오류를 출력합니다.
-if ($result === false) {
-    echo "socket_connect() failed: " . socket_strerror(socket_last_error($socket)) . "\n";
-    $this->error('055', __('Failed to connect to coin server. details2: ').socket_strerror(socket_last_error());
-}
-
-// 서버로 데이터를 전송합니다.
 $message = "02000200XXXXXXXX200132015071110421423           023           0000002OY   74312391143                         88    0000000000100test                0000000000000                             088";
+
+// TCP/IP 소켓 생성
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    echo "소켓 생성 실패: " . socket_strerror(socket_last_error()) . PHP_EOL;
+    exit;
+}
+
+// 서버에 연결
+$result = socket_connect($socket, $ip, $port);
+if ($result === false) {
+    echo "서버 연결 실패: " . socket_strerror(socket_last_error($socket)) . PHP_EOL;
+    exit;
+}
+
+// 서버로 메시지 전송
 socket_write($socket, $message, strlen($message));
 
-// 서버로부터 응답을 받습니다.
+// 서버로부터 응답 받기
 $response = socket_read($socket, 1024);
+echo "서버 응답: " . $response . PHP_EOL;
 
-// 서버로부터 받은 응답을 출력합니다.
-$res = "Response from server: " . $response . "\n";
-
-// 소켓을 닫습니다.
+// 소켓 닫기
 socket_close($socket);
-$this->error('055', __('Failed to connect to coin server. details3: ').$res);
 
-
-// response
-$tradeapi->success($res);
-
+$tradeapi->success($response);
 ?>
