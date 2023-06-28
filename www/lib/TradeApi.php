7ff8768cb6afcca9729c55028cef975201a9f316
kmg <flyminggo@naver.com>
2023-06-28 14:10:56 +0900

    u20

--- www/lib/TradeApi.php
+++ www/lib/TradeApi.php
@@ -3014,40 +3014,7 @@ SELECT
         return $GLOBALS['tradeapi']->checkMedia($s);
     }}
 
-    /**
-     *  accessToken
-     * * */
-    function publishToken($clientId, $clientSecret) {
-    //토큰수령
-    
-        $url = "https://oauth.codef.io/oauth/token";
-        $params = "grant_type=client_credentials&scope=read";
-    
-        $con = curl_init($url);
-        curl_setopt($con, CURLOPT_POST, true);
-        curl_setopt($con, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($con, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded"));
-    
-        $auth = $clientId . ":" . $clientSecret;
-        $authEncBytes = base64_encode($auth);
-        $authHeader = "Basic " . $authEncBytes;
-    
-        curl_setopt($con, CURLOPT_HTTPHEADER, array("Authorization: " . $authHeader));
-        curl_setopt($con, CURLOPT_POSTFIELDS, $params);
-    
-        $response = curl_exec($con);
-        $responseCode = curl_getinfo($con, CURLINFO_HTTP_CODE);
-        curl_close($con);
-    
-    
-        if ($responseCode == 200) {
-                $tokenMap = json_decode(urldecode($response), true);
-                return $tokenMap;
-        } else {
-                return null;
-        }
-    
-    }
+  
 
 
 }
