84bdfe0166f9d21e6a0866444673d8db03356ee6
kmg <flyminggo@naver.com>
2023-06-28 14:27:01 +0900

    u26

--- www/v1.0/checkAccount/index.php
+++ www/v1.0/checkAccount/index.php
@@ -42,7 +42,7 @@ $headers = array();
 
 if ($responseCode == 200) {
       $tokenMap = json_decode(urldecode($response), true);
-      //$tradeapi->error('049', __('토큰확인'. $tokenMap));
+      $tradeapi->error('049', __('토큰확인'. var_dump($tokenMap)));
       
        // 요청 헤더 설정
       $headers = array(
