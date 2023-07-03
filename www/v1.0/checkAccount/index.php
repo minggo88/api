77a84e7f1d7e086b642d6f2461e34ca4582b1306
kmg <flyminggo@naver.com>
2023-07-03 10:37:04 +0900

    httpcode 확인 2

--- www/v1.0/checkAccount/index.php
+++ www/v1.0/checkAccount/index.php
@@ -138,14 +138,15 @@ $bodyString = urlencode($bodyString);
    $httpCode = curl_getinfo($con, CURLINFO_HTTP_CODE);
    curl_close($con);
 
-   
+   $obj = json_decode($response);
+   $tradeapi->error('049', __('httpCode : '.$httpCode."///" $obj)); //내역확인용 강제 종료 알람   
 
    if ($httpCode == 200) { // 정상 응답
        $obj = json_decode($response);
-       $tradeapi->error('049', __('httpCode : '.$httpCode."///" $obj)); //내역확인용 강제 종료 알람   
+       
    } else { // 에러 발생
        $obj = json_decode($response);
-       $tradeapi->error('049', __('httpCode : '.$httpCode."///". $obj)); //내역확인용 강제 종료 알람   
+       
    }
 
    // 결과 반환
