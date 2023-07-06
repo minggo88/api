76a834a65f514bbcba9ebccacfc24933d7a95ad2
kmg <flyminggo@naver.com>
2023-07-06 18:42:17 +0900

    apicheck4

--- www/v1.0/checkAccount/index.php
+++ www/v1.0/checkAccount/index.php
@@ -270,7 +270,7 @@ $body = array(
    //$input['result'] = $result['data']['resTrHistoryList']['0'];
 
    //$tradeapi->error('049', __('API 요청 성공'. implode(" ", $data))); //주문수량을 잔여수량 이하로 입력해주세요.
-   $tradeapi->error('049', __($text)); //주문수량을 잔여수량 이하로 입력해주세요.
+   $tradeapi->error('049', __($result)); //주문수량을 잔여수량 이하로 입력해주세요.
  } else {
    //$tradeapi->error('ff', __('qqqqq.')); //주문수량을 잔여수량 이하로 입력해주세요.
    $tradeapi->error('049', __('API 요청 실패'. $httpCode. '  //  '. $response)); //주문수량을 잔여수량 이하로 입력해주세요.
