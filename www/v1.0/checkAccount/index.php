324e0571076899116b7689d7bcfd6ccec7a7d6d3
kmg <flyminggo@naver.com>
2023-07-06 20:06:13 +0900

    check15

--- www/v1.0/checkAccount/index.php
+++ www/v1.0/checkAccount/index.php
@@ -13,13 +13,14 @@ $tradeapi->set_db_link('master');
 $sql = "SELECT COUNT(*) FROM js_exchange_wallet_txn WHERE symbol = 'KRW' AND status = 'O';";
 
 $cnt = $this->query_fetch_object($sql);
-
+/*
 if($cnt >0){
    $sql = "SELECT txnid,userno,address_relative,amount FROM js_exchange_wallet_txn WHERE symbol = 'KRW' AND status = 'O';";
    $currencies = $tradeapi->query_list_object($sql);
 }
+*/
 
-$tradeapi->error('049', __($currencies));
+$tradeapi->error('049', __($cnt));
 
 
 // get my member information
