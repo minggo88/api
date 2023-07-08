4b34a4abfa5a7b9daff9803ae431183a75fb1ec1
kmg <flyminggo@naver.com>
2023-07-09 02:38:18 +0900

    go1

--- www/v1.0/checkAccount/index.php
+++ www/v1.0/checkAccount/index.php
@@ -40,11 +40,35 @@ if($cnt >0){
 
       $result = $txnid."/".$userno."/".$name."/".$amount;
       
-      $sql_income = "SELECT incomeIndex FROM js_income WHERE complteYN = 'N' AND js_income.resAccountDesc3 LIKE '%".$name."%' AND js_income.resAccountIn = '".$amount."' ORDER BY incomeIndex ASC LIMIT 1;";
-      $incomeIndex = $tradeapi->query_one($sql_income);
+      //입금 내역확인
+      $sql_income_search = "SELECT count(*) FROM js_income WHERE complteYN = 'N' AND js_income.resAccountDesc3 LIKE '%".$name."%' AND js_income.resAccountIn = '".$amount."';";
+      $income_cnt = $tradeapi->query_one($sql);
+      
+      //입금 내역이 있으면 진행
+      if($income_cnt>0){
+         $currentDateTime = date('Y-m-d H:i:s');
+         //income 업데이트
+         $sql_income_search = "SELECT incomeIndex FROM js_income WHERE complteYN = 'N' AND js_income.resAccountDesc3 LIKE '%".$name."%' AND js_income.resAccountIn = '".$amount."' ORDER BY incomeIndex ASC LIMIT 1;";
+         $incomeIndex = $tradeapi->query_one($sql_income_search);
 
+         $sql_income_update = "UPDATE kkikda.js_income SET userno='".$userno."', complteYN='Y', txnindex='".$txnid."' WHERE incomeIndex='".$incomeIndex."';";
+         $incomeIndex = $tradeapi->query_one($sql_income_update);
 
-      $tradeapi->error('049', __($incomeIndex));
+         //txn 업데이트
+         $text = "자동 업데이트 imcomeIndex : ".$incomeIndex;
+         $sql_txn_update = "UPDATE kkikda.js_exchange_wallet_txn SET txndate='".$currentDateTime."', status='D', msg='".$text."' WHERE txnid=".$txnid.";";
+         $tradeapi->query_one($sql_txn_update);
+         
+         ///userno에 돈집어넣는 로직
+
+         $sql_wallet_search = "SELECT confirmed FROM js_exchange_wallet WHERE userno = '".$userno."' AND symbol = 'KRW'";
+         $confirmed = $tradeapi->query_one($sql_wallet_search);
+         $confirmed = $confirmed + $amount;
+
+         $sql_wallet_update = "UPDATE kkikda.js_exchange_wallet SET confirmed=".$confirmed." WHERE userno='.$userno.' AND symbol='KRW';";
+         $tradeapi->query_one($sql_wallet_update);
+      }
+           
       
       //$amount = $currencies[$i].['amount'];
 
