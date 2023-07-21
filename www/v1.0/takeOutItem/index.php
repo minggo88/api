65d077fe15a9d92d28cb738227fad4291b99ac2f
kmg <flyminggo@naver.com>
2023-07-21 14:53:30 +0900

    data check2

--- www/v1.0/takeOutItem/index.php
+++ www/v1.0/takeOutItem/index.php
@@ -30,14 +30,15 @@ if (isset($_POST['dataArray'])) {
         $name = $data['name'];
         $symbol = $data['symbol'];
         $cnt = $data['cnt'];
-        $text += $name + $symbol + $cnt;
+        
         // 여기서부터는 $name, $code, $count 변수를 사용하여 원하는 작업을 수행할 수 있습니다.
         // 예를 들어, 데이터베이스에 저장하거나 다른 연산을 수행하는 등의 작업을 수행할 수 있습니다.
     }
+    $text = implode(', ', $dataArray);
 
     // Ajax 요청에 대한 응답을 줄 때는 JSON 형식으로 보내는 것이 일반적입니다.
     // 여기서는 간단한 응답을 준비하고 JSON 형식으로 보내는 예제를 보여드리겠습니다.
-    $tradeapi->error('001', __('여기까진 계획대로 : ' + $dataArray));
+    $tradeapi->error('001', __('여기까진 계획대로 : ' + $text));
 } else {
     $tradeapi->error('000', __('데이터형 오류'));
 }
