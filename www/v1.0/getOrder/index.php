ba915b28c45f5067463856256ceaf9ecfb4d6194
kmg <flyminggo@naver.com>
2024-12-09 09:28:48 +0900

    getOrder make

---
+++ www/v1.0/getOrder/index.php
@@ -0,0 +1,18 @@
+<?php
+include dirname(__file__) . "/../../lib/TradeApi.php";
+
+// 로그인 세션 확인.
+$tradeapi->checkLogin();
+//$userno = $tradeapi->get_login_userno();
+
+//메인반출내용
+$search_sql = 
+    "SELECT send_date, payment_type, payment, send_call, payment_name, receive_name, order_item, item_cnt,
+ 			order_item2, item_cnt2, order_item3, item_cnt3, order_item4, item_cnt4, order_item5, item_cnt5,
+ 			receive_call, box_cnt, receive_address_num, receive_address, receive_code, move, move_cnt, send_message,
+ 			order_index ,stime
+		 FROM js_test_order
+            ORDER BY send_date DESC;";
+$t_data = $tradeapi->query_list_object($search_sql);
+
+$tradeapi->success($t_data);
