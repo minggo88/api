350d9a3c90a9d2d12d0e718df7a03cb59f366c23
kmg <flyminggo@naver.com>
2024-12-10 09:51:42 +0900

    test2

--- www/v1.0/upOrder/index.php
+++ www/v1.0/upOrder/index.php
@@ -10,6 +10,7 @@ $item = $dataArray['order_item'];
 $search_sql = 
     "SELECT item_index from js_test_item WHERE i_value LIKE '%$item%';";
 $search_data = $tradeapi->query_list_object($search_sql);
+//$order_item = $search_data[0];
 
 
 $send_date= $dataArray['send_date'];
@@ -42,10 +43,10 @@ $item_cnt5 = $dataArray[0]['item_cnt5'];*/
 //메인반출내용
 $up_sql = 
     "UPDATE kkikda.js_test_order
-		SET payment_type='$payment_type', payment='$payment', payment_name='$payment_name', order_item='$search_data[0]',
+		SET payment_type='$payment_type', payment='$payment', payment_name='$payment_name', order_item='$search_data',
 			item_cnt='$item_cnt', send_name='$send_name', send_call='$send_call', send_address='$send_address', receive_address='$receive_address', 
 			receive_name='$receive_name', receive_call='$receive_call', receive_address_num='$receive_address_num', send_date='$send_date', 
-			box_cnt='$box_count', receive_code='$receive_code', move='$move', send_message='$send_message'
+			box_cnt='$box_count', receive_code='$receive_code', move='$move', send_message='$send_message' 
 		WHERE order_index='$order_index';";
 $t_data = $tradeapi->query_list_object($up_sql);
 
