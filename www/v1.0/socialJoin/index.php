e0859b35b479d7572a75075f1edff81b6fe977f0
kmg <flyminggo@naver.com>
2025-08-19 12:54:18 +0900

    header 오류 수정

--- www/v1.0/socialJoin/index.php
+++ www/v1.0/socialJoin/index.php
@@ -1,9 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: https://assettea.com');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Access-Control-Allow-Credentials: true');
-
 // OPTIONS 요청 처리
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
